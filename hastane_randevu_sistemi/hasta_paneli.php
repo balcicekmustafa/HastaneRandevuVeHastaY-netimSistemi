96cfd0c50db1d7b9027ac56e5eb9336206d60c40
Ayşe Koç <100020815+aysekocc@users.noreply.github.com>
2024-05-14 09:20:02 +0300

    Update hasta_paneli.php

--- hastane_randevu_sistemi/hasta_paneli.php
+++ hastane_randevu_sistemi/hasta_paneli.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "localhost";
+$servername = "localhost:3306";
 $username = "root";
 $password = "ayse+481";
 $database = "hastaneyonetimsisvt";
@@ -11,7 +11,7 @@ $baglanti = new mysqli($servername, $username, $password, $database);
 if ($baglanti->connect_error) {
     die("Bağlantı hatası: " . $baglanti->connect_error);
 } else {
-    echo "Veritabanına başarıyla bağlandı";
+    echo "";
 }
 session_start();
 include 'hasta_kayit.php';
